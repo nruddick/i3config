9bfaa58979b051a4fc02a3dd4e192d918a482d23
nruddick <nathanruddick@gmail.com>
2017-02-07 10:42:03 -0800

    picture

---
+++ rightside.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI2 --off --output HDMI1 --off --output DP1 --mode 1600x900 --pos 1366x0 --rotate normal --output eDP1 --primary --mode 1366x768 --pos 0x204 --rotate normal --output VIRTUAL1 --off
